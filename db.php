2a22fd4e3ebab3b1ddf052063b256919d874291e
Martin Gonzalez <martineduardogonzalez@gmail.com>
2021-12-08 09:17:35 -0300

    Refactoring elements ID

--- db.php
+++ db.php
@@ -1,16 +1,16 @@
 <?php
-
 session_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "scandinavia";
 
-$conn = mysqli_connect(
-    'localhost',
-    'root',
-    '',
-    'scandinavia'
-);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
 
-// if(isset($conn)){
-//     echo " Scandinavian database connected";
-// }
-
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// echo "Connected successfully";
 ?>
